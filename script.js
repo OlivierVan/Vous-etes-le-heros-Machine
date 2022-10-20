5ef6910ba6668ea5712eb82a53e6498a3f5718a5
OlivierVan <93718483+OlivierVan@users.noreply.github.com>
2022-10-20 14:11:10 -0400

    PS3.1/3.2

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let chaptersObj = {
         subtitle : "Le réveil",
         text : "Vous vous réveiller dans un monde ravagé par le temps et vous entendez un énorme bruit venant de loin, que faites-vous?",
         img : "",
-        options:[{option1 :{optionText: "Explorer", action:"goToChapter('chapitre2')"},
-                 option2 :{optionText: "Se cacher", action:"goToChapter('chapitre1mort')"},
-                 option3 :{optionText: "Aller vers le bruit", action:"goToChapter('chapitre2')"},},]},
+        options:[{optionText: "Explorer", action:"goToChapter('chapitre2')"},
+                 {optionText: "Se cacher", action:"goToChapter('chapitre1mort')"},
+                 {optionText: "Aller vers le bruit", action:"goToChapter('chapitre2')"},]},
     
                  
     
@@ -17,22 +17,22 @@ let chaptersObj = {
         subtitle : "Accident",
         text : "En vous cachant vous tombez et vôtre crâne se fracasse contre un roche, vous mourrez.",
         img : "",
-        options:[{option1 :{optionText: "Ouch", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Ouch", action:"goToChapter('chapitre1')"},]},
     
     
     chapitre2 : {
         subtitle : "Laboratoire",
         text : "Pendant que vous explorez les environs, vous trouvez un laboratoire, que faites-vous",
-        img : "",
-        options:[{options1 :{optionText: "Entrer", action:"goToChapter('chapitre3')"},
-                 options2 :{optionText: "Inspecter les environs", action:"goToChapter('chapitre2mort')"},},]},
+        img : "Assets/AI-Concept.jpg",
+        options:[{optionText: "Entrer", action:"goToChapter('chapitre3')"},
+                 {optionText: "Inspecter les environs", action:"goToChapter('chapitre2mort')"}]},
     
     
     chapitre2mort: {
         subtitle : "Embuscade",
         text : "Près de l'entrée du laboratoire quelque vous tends une embuscade, il vous cogne sur la tête et vous mourrez.",
         img : "",
-        options:[{option1 :{optionText: "Ouch!", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Ouch!", action:"goToChapter('chapitre1')"}]},
     
     
     
@@ -40,63 +40,63 @@ let chaptersObj = {
         subtitle : "Choix difficile",
         text : "Trois couloir ce trouve devant vous, vous devez faire un choix.",
         img : "",
-        options:[{option1 :{optionText: "Droite", action:"goToChapter('chapitre3clé')"},
-                 option2 :{optionText: "Gauche", action:"goToChapter('chapitre3mort')"},
-                 option3 :{optionText: "Avant", action:"goToChapter('chapitre4')"},},]},
+        options:[{optionText: "Droite", action:"goToChapter('chapitre3clé')"},
+                 {optionText: "Gauche", action:"goToChapter('chapitre3mort')"},
+                {optionText: "Avant", action:"goToChapter('chapitre4')"}]},
     
     
     chapitre3mort :{
         subtitle : "Piège",
         text : "Quelqu'un vous a tendu un piège, vous mourrez.",
         img : "",
-        options:[{option1 :{optionText: "Bing!", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Bing!", action:"goToChapter('chapitre1')"}]},
     
     
     chapitre3clé : {
         subtitle : "Clés",
         text : "Dans un bureau, vous trouvez des clés, mais vous mourrez en trébuchant.",
         img : "",
-        options:[{option1 :{optionText: "Bong!", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Bong!", action:"keytrue()"}]},
     
     
     chapitre4 : {
         subtitle : "La porte",
         text : "Devant vous, se tient une énorme porte circulaire, que faites-vous?",
         img : "",
-        options:[{option1 :{optionText: "Bong!", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Inspecter", action:"goToChapter('chapitre5clé')"},]},
     
     
     chapitre5clé : {
         subtitle : "Serrure",
-        text : "Avez-vous la clé",
+        text : "Vous tentez d'ouvrir la porte, mais vous remarquer qu'elle a une serrure",
         img : "",
-        options:[{options1 :{optionText: "Oui", action:"goToChapter('chapitre6')"},
-            options2 :{optionText: "Non", action:"goToChapter('chapitre5mort')"},},]},
+        options:[{optionText: "Avez-vous la clé", action:"haskey()"},
+                ]},
     
     chapitre5mort : {
         subtitle : "Mort stupide",
         text : "N'ayant pas la clé, vous ressortez dehors, mais vous vous faites écraser par un porte.",
         img : "",
-        options:[{option1 :{optionText: "Squash!", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Squash!", action:"goToChapter('chapitre1')"}]},
     
     chapitre6 : {
         subtitle : "Étonnement",
         text : "Vous entrez dans une salle rempli de machine d'apparence humaine, c'est machine vous ressemble drolement, que faites-vous?",
         img : "",
-        options:[{options1 :{optionText: "Enfuyez", action:"goToChapter('chapitre6mort')"},
-            options2 :{optionText: "Explorer", action:"goToChapter('chapitre7')"},},]},
+        options:[{optionText: "Enfuyez", action:"goToChapter('chapitre6mort')"},
+                {optionText: "Explorer", action:"goToChapter('chapitre7')"}]},
     
     chapitre6mort : {
         subtitle : "Panique",
         text : "Sous le choc après avoir vue ces machines, vous voulez sortir, mais vous vous enfargez et mourrez.",
         img : "",
-        options:[{option1 :{optionText: "Splish!", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Splish!", action:"goToChapter('chapitre1')"}]},
     
     chapitre7fin : {
         subtitle : "Vérité",
         text : "Vous découvrez la vérité sur vos origines. Les humains sont tous mort il y a cela des années, il ne reste que des machines doté d'intelligence artificielle.",
         img : "",
-        options:[{option1 :{optionText: "Recommencez", action:"goToChapter('chapitre1')"},},]},
+        options:[{optionText: "Recommencez", action:"goToChapter('chapitre1')"}]},
     }
     
     
@@ -106,4 +106,45 @@ let chaptersObj = {
         console.log(chaptersObj[chapterName]["text"]);
     
     };
+
+    goToChapter("chapitre1")
+
+function goToChapter(chapterName){
+
+    document.querySelector("h2").innerHTML=chaptersObj[chapterName]["subtitle"];
+    document.querySelector("p").innerHTML=chaptersObj[chapterName]["text"];
+    document.querySelector(".medias").src = chaptersObj[chapterName]["img"];
+    let btn = document.querySelector(".btn")
+   // document.querySelector(".btn").parentElement.removeChild(document.querySelector(".btn"));
+   btn.innerHTML = ""
+    for(element of chaptersObj[chapterName]["options"]){
+        let bouton = document.createElement("button")
+        let text = document.createTextNode(element["optionText"])
+        bouton.appendChild(text)
+        bouton.setAttribute("onclick" , element["action"])
+        bouton.setAttribute("type","button")
+        btn.appendChild(bouton)
+
+
+}
+};
     
+let keyFounded = false;
+
+function keytrue(){
+
+    keyFounded = true;
+    goToChapter("chapitre1")
+}
+
+function haskey(){
+
+    if(keyFounded==true){
+
+        goToChapter("chapitre6")
+    }
+    if(keyFounded==false){
+
+        goToChapter("chapitre1")
+    }
+}
